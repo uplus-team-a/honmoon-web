4d7ef8cc9f53f4fff93da30fddfd22f795747241
donghokang <donghokang.lguplus@gmail.com>
2025-08-06 10:06:23 +0900

    feat/quiz1: 퀴즈 미션 구현

--- src/app/my-profile/page.tsx
+++ src/app/my-profile/page.tsx
@@ -1,7 +1,7 @@
 import ProfilePage from "../../features/profile/components/ProfilePage";
 
 /**
- * 마이 프로필 페이지 라우트
+ * 마이 프 페이지 라우트...
  */
 export default function MyProfileRoute() {
     return <ProfilePage/>;
